b537f6ac37c7d459f3f87618df21d7c8da42f33c
tiger-404 <tigerspacetimego@gmail.com>
2025-05-04 20:53:06 +0800

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 namespace screenmagic{
-
-    
+    export function plotAt(index:number): void{
+        index |= 0
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot(x, y);
+    }
 }
